2c25f7e3f361fbd71ef9c29722d779c909c77a96
qintianhaohao <qintianhaohao@163.com>
2023-07-29 15:29:48 +0800

    add parallel sample use ray.remote

--- cartpole_ray_sample.py
+++ cartpole_ray_sample.py
@@ -5,24 +5,23 @@ import gym
 import ray
 
 
-
-@ray.remote
+@ray.remote(num_cpus=0.5)
 def cartpole_sample(args):
     env_id = args[0]
     reply_buffer = ray.get(args[1])
-    print(f'aaaaa: {reply_buffer}')
+    episodes = args[2]
+    print(f'reply buffer: {reply_buffer}')
 
     print(f'----- start env, id: {env_id}')
-    episode = 20000000
     env = gym.make("CartPole-v1")
 
-    for ep in range(episode):
+    for ep in range(episodes):
         obs = env.reset()
         while True:
             action = env.action_space.sample()
             next_obs, reward, done, info = env.step(action)
             # reply_buffer.add(obs, next_obs, action, reward, done, env_id)
-            reply_buffer[env_id] = env_id
+            # reply_buffer[env_id] = env_id
             obs = next_obs
             if done:
                 break
@@ -31,7 +30,7 @@ def cartpole_sample(args):
     return reply_buffer
 
 
-def run_ray_pool():
+def run_ray_pool(num_episodes_per_worker, num_workers):
     """
 
     :return:
@@ -39,15 +38,19 @@ def run_ray_pool():
 
     # buffer = multiprocessing.Array('i', 3)
     # buffer = array.array('i', [0]*3)
-    buffer_id = ray.put([0]*5)
+    buffer_id = ray.put([0] * num_workers)
     # buffer_id.flags.writeable = True
     # buffer_id = [0]*3
     # args = [(i, buffer_id) for i in range(10)]
 
-    print(ray.get([cartpole_sample.remote([i, buffer_id]) for i in range(5)]))
+    print(ray.get([cartpole_sample.remote([i, buffer_id, num_episodes_per_worker]) for i in range(num_workers)]))
 
 
 
 if __name__ == '__main__':
+    num_episodes_per_worker = 20000
+    num_workers = 8
+
     ray.init()
-    run_ray_pool()
+    
+    run_ray_pool(num_episodes_per_worker, num_workers)
